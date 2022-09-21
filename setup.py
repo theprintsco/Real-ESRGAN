1ec76ebeefc4cce39a7d707e2718f674a5529c78
Sreerag M <sreeragibtl@gmail.com>
2022-09-21 22:19:29 +0530

    dev: Add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name="REAL_ESRGAN_FORK",
+    version="0.1",
+    author="theprintsco",
+    author_email="contact@theprintscompany.com",
+    packages=["."],
+    install_requires=[numpy, 
+    opencv-python,
+    Pillow,
+    torch>=1.7,
+    torchvision>=0.8.0,
+    tqdm,
+    imageio,],
+)
