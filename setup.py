b24748b1e1113c7d0b4ca86953249350dc32fa4b
Sreerag M <sreeragibtl@gmail.com>
2022-09-21 22:30:27 +0530

    fix: Remove unnecessary

--- setup.py
+++ setup.py
@@ -5,11 +5,8 @@ setup(
     version='0.1.0',
     author='J. Random Hacker',
     author_email='jrh@example.com',
-    packages=['Real-ESRGAN'],
     scripts=['realesrgan.py','rrdbnet_arch.py', 'main.py'],
     url='http://pypi.python.org/pypi/TowelStuff/',
-    license='LICENSE.txt',
     description='Useful towel-related stuff.',
-    long_description=open('README.txt').read(),
     install_requires=open('requirements.txt').read()
 )
