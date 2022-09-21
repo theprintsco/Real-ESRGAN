9c594276ebb694b91802b5141e4ebfc79bd3305f
Sreerag M <sreeragibtl@gmail.com>
2022-09-21 22:29:15 +0530

    dev: Update setup

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name="REAL_ESRGAN_FORK",
-    version="0.1",
-    author="theprintsco",
-    author_email="contact@theprintscompany.com",
-    packages=["Real-RealESRGAN"],
-    install_requires=[numpy, 
-    opencv-python,
-    Pillow,
-    torch>=1.7,
-    torchvision>=0.8.0,
-    tqdm,
-    imageio,]
+    name='TowelStuff',
+    version='0.1.0',
+    author='J. Random Hacker',
+    author_email='jrh@example.com',
+    packages=['Real-ESRGAN'],
+    scripts=['realesrgan.py','rrdbnet_arch.py', 'main.py'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='Useful towel-related stuff.',
+    long_description=open('README.txt').read(),
+    install_requires=open('requirements.txt').read()
 )
