eb40bcbbde4730fb3e5f028ff95d01eef60deb23
Sreerag M <sreeragibtl@gmail.com>
2022-09-23 09:56:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author_email='x@y.com',
     url='https://github.com/sberbank-ai/Real-ESRGAN',
     description='Real-ESRGAN',
-    install_requires=open('requirements.txt').read()
+    install_requires=open('requirements.txt').read(),
     py_modules=['realesrgan', 'rrdbnet_arch', 'main', 'arch_util', 'utils_sr']
 )
